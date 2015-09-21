50ff180d7043a12549496af508eecd1907b6d9ad
Michael Reed <myk27x@yahoo.com>
2015-09-21 16:03:47 -0400

    fixed second error, returns expected array. second test passing

--- green_eggs_and_ham.rb
+++ green_eggs_and_ham.rb
@@ -2,14 +2,16 @@ class GreenEggsAndHam
 
   def initialize
     @text = File.read('green_eggs_and_ham.txt')
+    @words = @text.split
   end
 
   def word_count
-    @words = @text.split
     @words.count
   end
 
   def sorted_unique_words
-    
+    @words.map { |word| word.downcase.gsub(/[.!?,]/, "")}.
+           uniq.
+           sort
   end
 end
