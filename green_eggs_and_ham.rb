cff415fab6f75a541c6cc19ba594e64c47667c48
Michael Reed <myk27x@yahoo.com>
2015-09-21 21:05:56 -0400

    fixed second error, returns hash as expected. fifth test passing. refactored for redundancy.

--- green_eggs_and_ham.rb
+++ green_eggs_and_ham.rb
@@ -2,30 +2,29 @@ class GreenEggsAndHam
 
   def initialize
     @text = File.read('green_eggs_and_ham.txt')
-    @words = @text.split
+    @raw_words = @text.split
+    @down_and_out = @raw_words.map { |word| word.downcase.gsub(/[.!?,]/, "")}
   end
 
   def word_count
-    @words.count
+    @raw_words.count
   end
 
   def sorted_unique_words
-    @words.map { |word| word.downcase.gsub(/[.!?,]/, "")}.
-           uniq.
-           sort
+    @down_and_out.uniq.sort
   end
 
   def number_of_words_shorter_than(number)
-    @words.map { |word| word.downcase.gsub(/[.!?,]/, "")}.
-           select { |word| word.length < (number)}.
-           count
+    @down_and_out.select { |word| word.length < (number)}.count
   end
 
   def longest_word
-    @words.max_by { |word| word.length }
+    @raw_words.max_by { |word| word.length }
   end
 
   def frequency_of_unique_words
-    
+    @down_and_out.group_by { |word| word }.
+                  map { |word, frequency| [word, frequency.count] }.
+                  to_h
   end
 end
